93eb496ef63e3814cce5119de1a84cfb10b45fc4
Slooow68 <sloow2806@gmail.com>
2024-12-15 17:47:40 +0100

    add engines and agencies JSON file read

--- python_project_template-main/tests/py_project_5a_vb/test_data_loader.py
+++ python_project_template-main/tests/py_project_5a_vb/test_data_loader.py
@@ -1,11 +1,19 @@
 import json
 
 def load_data(file_name):
-    """Charge les données depuis un fichier JSON."""
+    """
+    Charge les données depuis un fichier JSON.
+    """
     with open(file_name, 'r') as f:
         return json.load(f)
 
 def get_rockets():
     return load_data('database/rockets.json')
+
+def get_engines():
+    return load_data('database/engines.json')
+
+def get_agencies():
+    return load_data('database/agencies.json')
 
 
